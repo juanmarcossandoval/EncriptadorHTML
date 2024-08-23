46c7a050e8d7f174123eb5a8325dc9907eff4531
juanmarcossandoval <juanmarcos.sandoval@gmail.com>
2024-08-23 08:19:32 -0300

    style improvement

--- script.js
+++ script.js
@@ -24,9 +24,49 @@ function flipButtons(){
     var encriptar = document.getElementById('Encriptador');
     var desencriptar = document.getElementById('Desencriptador');
     modo ? encriptar.setAttribute("class","button bgc-blue") : encriptar.setAttribute("class","button bgc-grey");
-    !modo ? desencriptar.setAttribute("class","button bgc-green") : desencriptar.setAttribute("class","button bgc-grey");
+    !modo ? desencriptar.setAttribute("class","button bgc-red") : desencriptar.setAttribute("class","button bgc-grey");
     modo ? encriptar.disabled = false : encriptar.disabled = true;
     !modo ? desencriptar.disabled = false : desencriptar.disabled = true;
 }
 
 flipButtons();
+
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+var letters = 'ラドクリフマラソンわたしワタシんょンョたばこタバコとうきょうトウキョウ ';
+letters = letters.split('');
+
+var fontSize = 10,
+    columns = canvas.width / fontSize;
+
+var drops = [];
+for (var i = 0; i < columns; i++) {
+  drops[i] = 1;
+}
+
+function draw() {
+  ctx.fillStyle = 'rgba(0, 0, 0, 0.1)';
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  for (var i = 0; i < drops.length; i++) {
+    var text = letters[Math.floor(Math.random() * letters.length)];
+    ctx.fillStyle = '#0f0';
+    ctx.fillText(text, i * fontSize, drops[i] * fontSize);
+    drops[i]++;
+    if (drops[i] * fontSize > canvas.height && Math.random() > .95) {
+      drops[i] = 0;
+    }
+  }
+}
+
+setInterval(draw, 60);
+
+function copyToClipboard() {
+    var copyText = document.getElementById('__mensaje');
+    copyText.select();
+    copyText.setSelectionRange(0, 99999);
+    navigator.clipboard.writeText(copyText.value);
+}
